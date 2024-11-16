40b9822f123604821a10b68fa9018a24e94e7606
MD. JOBAYER ARAFAT <mdjobayerarafat@gmail.com>
2024-11-16 22:52:45 +0600

    design

--- TodoApp/app/src/main/java/com/mdjobayerarafat/todoapp/TodoListPage.kt
+++ TodoApp/app/src/main/java/com/mdjobayerarafat/todoapp/TodoListPage.kt
@@ -2,14 +2,42 @@ package com.mdjobayerarafat.todoapp
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun TodoListPage(){
     val todoList = getFakeTodo()
-    Text(text = todoList.toString())
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(8.dp)
+    ) {
+        LazyColumn(
+            content = {
+                itemsIndexed(todoList){index:Int, item: Todo ->
+                    TodoIteam(item = item)
+                }
+            }
+        )
+    }
 
 
+}
+
+@Composable
+fun TodoIteam(item: Todo){
+   Row {
+
+   }
 }
