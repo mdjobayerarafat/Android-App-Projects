fa260987033eb27c34a7d83f06a24f56d3868779
MD. JOBAYER ARAFAT <mdjobayerarafat@gmail.com>
2024-11-16 22:37:10 +0600

    gettodo list

--- TodoApp/app/src/main/java/com/mdjobayerarafat/todoapp/TodoListPage.kt
+++ TodoApp/app/src/main/java/com/mdjobayerarafat/todoapp/TodoListPage.kt
@@ -1,8 +1,15 @@
 package com.mdjobayerarafat.todoapp
 
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun TodoListPage(){
+    val todoList = getFakeTodo()
+    Text(text = todoList.toString())
+
 
 }
